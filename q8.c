3f7ba7667ea71c6a50e8987df2e43e9784b061eb
LucasDuduardo <123954208+LucasDuduardo@users.noreply.github.com>
2023-02-13 14:27:32 -0300

    Create q8.c

---
+++ q8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a,b;
+
+    puts("digite um valor para saber seu valor ao quadrado");
+    scanf("%d",&a);
+    printf("o termo %d, ao quadrado de %d",a,a*a);
+   
+    return 0;
+
+}
