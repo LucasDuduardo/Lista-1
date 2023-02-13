339e558b33ba7b549b17ddaf45eb9c5aa7193896
LucasDuduardo <123954208+LucasDuduardo@users.noreply.github.com>
2023-02-13 13:59:34 -0300

    Create q3.c

---
+++ q3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    float a;
+    puts("insira o valor desejado:");
+    scanf("%f",&a);
+    printf("valor com uma casa decimal: %.1f\n",a);
+
+    return 0;
+
+
+}
