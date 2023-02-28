d9d5374fd87ce449cdbd0cedccd12356f9872d2a
LucasDuduardo <123954208+LucasDuduardo@users.noreply.github.com>
2023-02-28 20:09:21 -0300

    Update q26.c

--- q26.c
+++ q26.c
@@ -7,10 +7,12 @@ int main(){
 
     printf("insira uma coordenada para X1: ");
     scanf("%.2f %.2f",&x1,&x2);
+    fflush(stdin);
     
     printf("insira uma coordenada para Y1: ");
     scanf("%.2f %.2f",&y1,&y2);
-
+    fflush(stdin);
+    
     dist = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 
     printf("a uma distancia do ponto x(%.2f,%.2f) e y(%.2f,%.2f) é de %.2f",x1,x2,y1,y2,dist);
