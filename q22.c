ad57f41aefee21ad990ecdb0e4e2f7350d62b878
LucasDuduardo <123954208+LucasDuduardo@users.noreply.github.com>
2023-02-28 19:14:15 -0300

    Create q22.c

---
+++ q22.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("inteiro equivalente a: A\n");
+    printf("%d\n",'A');
+
+    printf("inteiro equivalente a: B\n");
+    printf("%d\n",'B');
+
+    printf("inteiro equivalente a: C\n");
+    printf("%d\n",'C');
+
+    printf("inteiro equivalente a: a\n");
+    printf("%d\n",'a');
+
+    printf("inteiro equivalente a: b\n");
+    printf("%d\n",'b');
+
+    printf("inteiro equivalente a: c\n");
+    printf("%d\n",'c');
+
+    printf("inteiro equivalente a: 0\n");
+    printf("%d\n",'0');
+
+    printf("inteiro equivalente a: 1\n");
+    printf("%d\n",'1');
+
+    printf("inteiro equivalente a: 2\n");
+    printf("%d\n",'2');
+
+    printf("inteiro equivalente a: $\n");
+    printf("%d\n",'$');
+
+    printf("inteiro equivalente a: *\n");
+    printf("n%d\n",'*');
+
+    printf("inteiro equivalente a: +\n");
+    printf("%d\n",'+');
+
+    printf("inteiro equivalente a: /\n");
+    printf("%d\n",'/');
+
+
+
+    return 0;
+}
